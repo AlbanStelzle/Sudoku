7cf2a5eef353b59f4aab7274af428f5622ab1755
stelzle <alban.stelzle2000@gmail.com>
2020-03-31 19:10:19 +0200

    Menu de selection pour mode manuel et automatique (pour plus tard)

--- SelectMode.java
+++ SelectMode.java
@@ -1,6 +1,9 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+import java.awt.Font;
 
 public class SelectMode{
 
@@ -9,17 +12,26 @@ public class SelectMode{
         JButton automatique = new JButton("AUTOMATIQUE");
         JButton manuel = new JButton("MANUEL");
         JPanel panneau = new JPanel();
+        JLabel titre = new JLabel("SELECTIONNEZ UN MODE");
+        Font policeTitre = new Font("Arial", Font.BOLD, 20);
 
         panneau.setLayout(null);
 
+        titre.setFont(policeTitre);
+        titre.setHorizontalAlignment(JLabel.CENTER);
+
+        titre.setLocation(150, 50);
         automatique.setLocation(150, 100);
         manuel.setLocation(150, 300);
 
+        titre.setSize(200, 20);
         automatique.setSize(200, 20);
         manuel.setSize(200, 20);
+
         automatique.addActionListener(ctrl);
         manuel.addActionListener(ctrl);
 
+        panneau.add(titre);
         panneau.add(automatique);
         panneau.add(manuel);
         panneau.setSize(fenetre.getWidth(), fenetre.getHeight());
---
+++ SelectModeEvent.java
@@ -0,0 +1,23 @@
+import java.awt.event.*;
+import javax.swing.JFrame;
+
+public class SelectModeEvent implements ActionListener {
+    private JFrame fenetre;
+    private String nomFichier;
+
+    public SelectModeEvent(JFrame fen, String filename) {
+        this.fenetre = fen;
+        this.nomFichier = filename;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String button;
+        button = e.getActionCommand();
+        if (button == "AUTOMATIQUE") {
+
+
+        } else if (button == "MANUEL") {
+
+    }
+
+}
